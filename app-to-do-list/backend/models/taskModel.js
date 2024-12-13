76ee8838e5a1697c65eb2354124a1758bed8b6c5
Nyito <49852550+Nyito@users.noreply.github.com>
2024-12-12 21:42:13 -0300

    created backend model and routes

---
+++ app-to-do-list/backend/models/taskModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String },
+        status: {
+            type: String,
+            required: true,
+            enum: ['pending', 'on going', 'finished']
+        },
+        finishedDate: { type: Date },
+        priority: {
+            type: String,
+            required: true,
+            enum: ['low', 'medium', 'high']
+        },
+        deadline: { type: Date },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
